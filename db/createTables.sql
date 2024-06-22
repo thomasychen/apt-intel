1e3db5815752ff5b76669db73a44069f05cb823c
Michael Wu <michaelwu21@gmail.com>
2024-06-22 15:41:13 -0700

    add db

---
+++ db/createTables.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS Apartments;
+
+CREATE TABLE Apartments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    bed INT NOT NULL,
+    bath INT NOT NULL,
+    cost INT NOT NULL,
+    description TEXT NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    sqft INT,
+    phone VARCHAR(255),
+    email VARCHAR(255),
+    url VARCHAR(255),
+    gender INT,
+    shared INT,
+    furnished INT,
+    pets INT,
+    parking INT,
+    laundry INT
+);
